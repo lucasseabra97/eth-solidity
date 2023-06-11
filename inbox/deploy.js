5faed4a77985bf69ae9a953b2af5899cf70cf7de
lucasseabra97 <lucasseabra@ua.pt>
2023-06-11 18:02:24 +0100

    feat: attempting to deploy to a testnetwork

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,25 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'valve view sight theme stairs series home volume banner salute since switch',
+    'https://goerli.infura.io/v3/f55e20f9507e4d969ea8074af971016a'
+);
+const web3 = new Web3(provider);
+
+const deploy = async() => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] })
+
+    console.log('Contract deploy to ', result.options.address)
+    provider.engine.stop()
+}
+
+deploy();
